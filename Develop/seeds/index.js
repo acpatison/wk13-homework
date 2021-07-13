489f6a1c01775061cb1c6c56ad6d7f3607906403
acpatison@gmail.com <acpatison@gmail.com>
2021-07-13 18:15:19 -0400

    changed seed contents

--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -8,11 +8,14 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
+  
+  await seedProducts();
+  console.log('\n----- PRODUCTS SEEDED -----\n');
+  
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  
 
   await seedTags();
   console.log('\n----- TAGS SEEDED -----\n');
