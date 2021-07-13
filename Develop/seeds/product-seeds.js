8b4fcad919661eff10809ecb2418771ca502ee5a
acpatison@gmail.com <acpatison@gmail.com>
2021-07-13 18:16:11 -0400

    changed seed contents

--- Develop/seeds/product-seeds.js
+++ Develop/seeds/product-seeds.js
@@ -2,16 +2,16 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
-    price: 14.99,
-    stock: 14,
-    category_id: 1,
-  },
-  {
     product_name: 'Running Sneakers',
     price: 90.0,
     stock: 25,
     category_id: 5,
+  },
+  {
+    product_name: 'Plain T-Shirt',
+    price: 14.99,
+    stock: 14,
+    category_id: 1,
   },
   {
     product_name: 'Branded Baseball Hat',
